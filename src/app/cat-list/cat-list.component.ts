d159f5514e74ad34382ccb516f79de95577d63ac
Minesh <mineshmshah@hotmail.co.uk>
2017-11-10 11:47:37 +0000

    added first service

---
+++ src/app/cat-list/cat-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-list',
+  templateUrl: './cat-list.component.html',
+  styleUrls: ['./cat-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CatListComponent implements OnInit {
+  cats = [
+    'Monty',
+    'Blanco',
+    'Arthur',
+    'Fatso'
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
